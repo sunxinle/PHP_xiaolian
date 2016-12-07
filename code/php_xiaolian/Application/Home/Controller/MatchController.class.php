20afa81036c7ec033d9f7b7b5a113b04ed4b6995
sunxinle <529851184@qq.com>
2016-12-07 21:41:47 +0800

    发送请求已经完成可以查看  孙新乐

--- code/php_xiaolian/Application/Home/Controller/MatchController.class.php
+++ code/php_xiaolian/Application/Home/Controller/MatchController.class.php
@@ -26,23 +26,26 @@ class MatchController extends Controller
         $this->assign('university',$university);
         $this->display();
     }
+    /*作者：孙新乐
+    时间：2016/12/07*/
     public function successTips(){
         $StudentMsg =I('post.');
-       // dump($StudentMsg);
-        $date=array();
-        $date['university'] =$StudentMsg[University];
-        $date[] =$StudentMsg[ReachTime];
-        $date[] =$StudentMsg[NoteMsg];
-       // dump($date);
+        $stu['id']=1;//发送者id
+        $school['uniname']=$StudentMsg[University];
+        $user=M('user');
         $match=M('match');
         $university=M('university');
-        $user =D('User');
-        $user->select();
-        //$user->relation('University')->select();
-       // $date['funiid']=$university->where(uniname=$StudentMsg[university]);
-    /*    dump($date['funiid']);
+        $date=array();
+        $date['tuniid']= $university->where($school)->getField("uniid");
+        $date['funiid']=$user->where($stu)->getField('uniid');
+        $date['fid']=$stu['id'];//发送者id
+        $date['tagone']=0;
+        $date['tagtwo']=0;
+        $date['msendtime']=date("Y-m-d H:i:s",time());//发送请求时间
+        $date['mgotime'] =$StudentMsg[ReachTime];//想要去的时间
+        $date['mmessage'] =$StudentMsg[NoteMsg];//备注信息
+      //dump($date['msendtime']);
         $match->add($date);
-        echo $match->getLastSql();*/
         $this->display();
     }
 }
