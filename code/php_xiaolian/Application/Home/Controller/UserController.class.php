057a754fe4ea1fbac990452dffa7fdac60666ace
wanglin2014 <2513418126@qq.com>
2016-12-08 08:28:46 +0800

    改动UserController.class.php

--- code/php_xiaolian/Application/Home/Controller/UserController.class.php
+++ code/php_xiaolian/Application/Home/Controller/UserController.class.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 郭颖
- * Date: 2016/11/30
- * Time: 15:24
- */
 namespace Home\Controller;
 
 use Org\Util\Rbac;
@@ -68,16 +62,14 @@ class UserController extends Controller {
         $college = I('post.college');
         dump($college);
         $name = I('post.name');
-        $sex = I('post.sex');
         $phonenumber = I('post.phonenumber');
         $sno = I('post.sno');
         $openid = I('post.openid');
 
-        $user = M('user');
+        $user = M('xl_user');
         $data = array();
         $data['college'] = $college;
         $data['name'] = $name;
-        $data['sex'] = $sex;
         $data['phonenumber'] = $phonenumber;
         $data['sno'] = $sno;
         //thinkphp的数据写入操作使用add方法
@@ -105,6 +97,33 @@ class UserController extends Controller {
                 //错误页面的默认跳转页面是返回前一页javascript:history.back(-1)，通常不需要设置
                 $this->error('提交失败');
             }
+        }
+    }
+
+
+    public function logout() {
+        session(null);
+        $this->success('退出成功！请重新登录',U('home/root/login'));
+    }
+
+    public function dologin() {
+        // 构造condition条件
+        $condition = array();
+        $condition['name'] = I('post.username');
+        $condition['passwd'] = I('post.password', '', 'md5');
+        // 调用RBAC方法实现用户校验
+        $authInfo = Rbac::authenticate($condition);
+        if ($authInfo) {    // 用户名和密码成立
+            // 写入session数据（自由修改）
+            session('loginedUserName', $authInfo['name']);
+            // 写入权限认证识别号
+            session(C('USER_AUTH_KEY'), $authInfo['id']);
+            // 在session中写入当前角色的权限列表
+            Rbac::saveAccessList($authInfo['id']);
+            // 登录成功后的跳转
+            $this->success('登录成功！', '/Admin');
+        } else {
+            $this->error('用户名或密码错误，请重新登录！');
         }
     }
 }
