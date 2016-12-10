31b288a5c5c8d0b00ea33eb57b00d3e99b9d6ada
sunxinle <529851184@qq.com>
2016-12-10 19:49:20 +0800

    聊天注册50%

--- code/php_xiaolian/Application/Home/Controller/UserController.class.php
+++ code/php_xiaolian/Application/Home/Controller/UserController.class.php
@@ -69,7 +69,7 @@ class UserController extends Controller {
         $sno = I('post.sno');
         $openid = I('post.openid');
 
-        $user = M('xl_user');
+        $user = M('user');
         $data = array();
         $data['college'] = $college;
         $data['name'] = $name;
@@ -83,7 +83,7 @@ class UserController extends Controller {
             if ($result){
                 //设置成功后跳转页面的地址，默认的返回页面是$_SERVER['HTTP_REFERER']
                 //默认等待时间success方法是1秒，error方法是3秒
-                $this->success('提交成功');
+                $this->success('提交成功',U('home/news/index'));
             } else {
                 //错误页面的默认跳转页面是返回前一页javascript:history.back(-1)，通常不需要设置
                 $this->error('提交失败');
@@ -103,30 +103,4 @@ class UserController extends Controller {
         }
     }
 
-
-    public function logout() {
-        session(null);
-        $this->success('退出成功！请重新登录',U('home/root/login'));
-    }
-
-    public function dologin() {
-        // 构造condition条件
-        $condition = array();
-        $condition['name'] = I('post.username');
-        $condition['passwd'] = I('post.password', '', 'md5');
-        // 调用RBAC方法实现用户校验
-        $authInfo = Rbac::authenticate($condition);
-        if ($authInfo) {    // 用户名和密码成立
-            // 写入session数据（自由修改）
-            session('loginedUserName', $authInfo['name']);
-            // 写入权限认证识别号
-            session(C('USER_AUTH_KEY'), $authInfo['id']);
-            // 在session中写入当前角色的权限列表
-            Rbac::saveAccessList($authInfo['id']);
-            // 登录成功后的跳转
-            $this->success('登录成功！', '/Admin');
-        } else {
-            $this->error('用户名或密码错误，请重新登录！');
-        }
-    }
 }
