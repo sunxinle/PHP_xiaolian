89292ae64180832326f9b4e3403da1a70e71a8e2
guoying2026 <2473548838@qq.com>
2016-12-12 21:27:47 +0800

    1

--- code/php_xiaolian/Application/Home/Controller/UserController.class.php
+++ code/php_xiaolian/Application/Home/Controller/UserController.class.php
@@ -48,15 +48,16 @@ class UserController extends Controller {
             //dump($result2);
             if(curl_errno()==0){
                 $data=json_decode($result2);
-                $openid=$data->openid; //openi数据
-                $nickname=$data->nickname; //名字
-                $sex=$data->sex;        //性别
-                $language=$data->language;     //语言
-                $city=$data->city;                 //城市
-                $province=$data->province;          //省份
-                $country=$data->country;            //国家
-                $imageurl=$data->headimgurl;
-
+                $openid=$data->openid;      //openid数据
+                $nickname=$data->nickname;  //名字
+                $sex=$data->sex;            //性别
+                $language=$data->language;  //语言
+                $city=$data->city;          //城市
+                $province=$data->province;  //省份
+                $country=$data->country;    //国家
+                $imageurl=$data->headimgurl;//头像
+                $ro['openid']=$openid;
+                if (!$user->where($ro)->find()){
                 //dump(json_decode($result2));
                 //把用户信息分配到视图文件中
                 $this->assign('openid',$openid);
@@ -123,30 +124,4 @@ class UserController extends Controller {
         }
     }
 
-
-    public function logout() {
-        session(null);
-        $this->success('退出成功！请重新登录',U('home/root/login'));
-    }
-
-    public function dologin() {
-        // 构造condition条件
-        $condition = array();
-        $condition['name'] = I('post.username');
-        $condition['passwd'] = I('post.password', '', 'md5');
-        // 调用RBAC方法实现用户校验
-        $authInfo = Rbac::authenticate($condition);
-        if ($authInfo) {    // 用户名和密码成立
-            // 写入session数据（自由修改）
-            session('loginedUserName', $authInfo['name']);
-            // 写入权限认证识别号
-            session(C('USER_AUTH_KEY'), $authInfo['id']);
-            // 在session中写入当前角色的权限列表
-            Rbac::saveAccessList($authInfo['id']);
-            // 登录成功后的跳转
-            $this->success('登录成功！', '/Admin');
-        } else {
-            $this->error('用户名或密码错误，请重新登录！');
-        }
-    }
 }
